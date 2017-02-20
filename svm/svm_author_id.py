e01c1670aa759086ce2fdff48a40552c72b059eb
tousept <tousept@163.com>
2017-02-20 20:53:35 +0800

    'decisiontree'

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10000.)
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print acc
+
+num = 0
+for idx in range(len(pred)):
+    if pred[idx] == 1:
+        num += 1
+
+print num
 #########################################################
 
 
